64150e8826361921e09d1c89a9d2c44c39805dba
Cheryl K <cherylkelson@icloud.com>
2018-11-16 14:56:44 +0000

    remove type of race

--- lib/triathlon/cli.rb
+++ lib/triathlon/cli.rb
@@ -5,26 +5,22 @@ module Triathlon
     class Error < StandardError; end
     def call
       Triathlon::Scraper.new.make_triathlons
-      puts "Welcome to the amazing world of competitive triathlons."
+      puts "Welcome. "
       start
     end
 
     def start
       puts ""
-      puts "In what type of race would you like to compete? Sprint or Olympic?"
+      puts "In which city would you like to race?"
       input = gets.strip
 
       print_races(input)
 
-      puts ""
-      puts "In which city would you like to compete?
-      input = gets.strip
-
       race = Triathlons::Triathlon.find(input)
 
-      print_race(race)
-      puts ""
-      puts "Would you like to see a race in another city?  Enter Y or N"
+      #print_race(race)
+      #puts ""
+      #puts "Would you like to see a race in another city?  Enter Y or N"
 
       input = gets.strip.downcase
       if input == "Y"
@@ -43,10 +39,9 @@ module Triathlon
 
       def print_races(race)
         puts""
-        puts"-----------#{race.type} - #{race.city}--------
+        puts" #{race.city}
 
         puts ""
-        puts "Type:   #{race.type}"
         puts "City: #{race.city}"
         puts "Website:  #{race.website_url}"
       end
