5fbf2f518a0ff45e82eec536d4bcd47c1f06df52
Cheryl K <cherylkelson@icloud.com>
2018-11-19 15:05:18 +0000

    add six regions for user to select location in class scraper

--- lib/triathlon/scraper.rb
+++ lib/triathlon/scraper.rb
@@ -1,14 +1,47 @@
 class Triathlon::Scraper
+require_relative '../lib/scraper.rb'
+require_relative "../lib/student.rb"
+require_relative "./triathlon/version"
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
+require 'colorize'
 
-  def get_page
-    Nokogiri:: HTML (open(XXXXXXX))
+class CommandLineInterface
+  BASE_PATH = "./fixtures/student-site/"
+
+
+  def get_page(page)
+    Nokogiri:: HTML (open(URL))
   end
 
-  def scrape_xxxx_index
-    self.get_page.css(xxxxxx)
+
+  def scrape (index)
+    gets.strip www.teamusa.org/usa-triathlon
+    self.get_page.css(index)
   end
 
-  def make_races
+
+  def display_races(races)
+    Races.all.each do |race|
+      puts "#{race.location.upcase}". colorize :(blue)
+      puts "#{location:".colorize(:ligth_blue) + "#{{race.location}"
+  end
+
+
+Midwest Region Scrape:  <a href="/USA-Triathlon/Regions/Mideast" target="_self">Mideast</a>
+Northeast Region Scrape: <a href="/USA-Triathlon/Regions/Northeast" target="_self">Northeast</a>
+North Central Region Scrape: <a href="/USA-Triathlon/Regions/North-Central" target="_self">North Central</a>
+Southeast Region Scrape: <a href="/USA-Triathlon/Regions/Southeast" target="_self">Southeast</a>
+South Central Region Scrape:  <a href="/USA-Triathlon/Regions/South-Central" target="_self">South Central</a>
+Western Region Scrape:  <a href="/USA-Triathlon/Regions/West" target="_self">West</a>
+
+
+end
+
+
+
+
     scrape_races_index.each do |r|
       xxxxxxxxxxx
     end
