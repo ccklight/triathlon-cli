362a1380706eabbfb6650653d60524e121bb1305
Cheryl K <cherylkelson@icloud.com>
2018-11-19 14:39:39 +0000

    refactor def self.all to reflect race array iteration

--- lib/races.rb
+++ lib/races.rb
@@ -4,9 +4,11 @@ class Triathlon::Races
   attr_reader :website
 
   @@all = []
+  @race = race
 
   def self.new_from_index_page(r)
     self.new(
+    @@all << self  # this is a class constructor
 
 
     )
@@ -15,22 +17,24 @@ class Triathlon::Races
   def initialize
       @location = location(location)
       @website = website(website)
+      @@all << self
   end
 
   def self.all
     @@all
-  end
-
-  def self.find(id)
-
+    race.new.select(Race.all "views/races/index.html", "site/index.html")
+    self.races.collect {|r| r.location}.uniq
+    self.location
   end
 
 
   def location
     1. 2019 High School National Championships Tempe, AZ
-    2. 2019 Duathlon National Championships Piedmont, SC 
+    2. 2019 Duathlon National Championships Piedmont, SC
   end
 
   def website
+    gets.strip www.teamusa.org/usa-triathlon
+    puts "the website information that is scraped"
 
   end
