4c6aa27d7bd54c88c8ce058c8c500e4a93003759
Cheryl K <cherylkelson@icloud.com>
2018-11-20 14:55:38 +0000

    remove incorrect require_relative

--- config/environment.rb
+++ config/environment.rb
@@ -1,13 +1,14 @@
 require_relative '../lib/scraper.rb'
-require_relative "../lib/student.rb"
 require_relative "./triathlon/version"
+require "triathlon/cli"
+
 require 'pry'
 require 'nokogiri'
 require 'open-uri'
 require 'colorize'
 
 require "bundler/setup"
-require "triathlon/cli"
+
 
 bundle install
 
