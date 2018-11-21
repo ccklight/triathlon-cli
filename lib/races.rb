490eb760bfb80e164d7d8ac4e0658f56280f13d0
Cheryl K <cherylkelson@icloud.com>
2018-11-21 14:18:57 +0000

    refactor self.all

--- lib/races.rb
+++ lib/races.rb
@@ -15,6 +15,23 @@ class Race
     @race_website = website
     @@all_races << self
 
+  def self.all
+    #Goal is to return my list of all of the races
+    @@all
+    self.locationdef self.new_from_url
+    self.race.collect
+  end
+
+  def location
+    1. 2019 High School National Championships Tempe, AZ
+    2. 2019 Duathlon National Championships Piedmont, SC
+  end
+
+  def website
+    gets.strip.www.teamusa.org/USA-Triathlon
+    puts "the website information that is scraped."
+  end
+
 
 
 
